f05a9015447d3eff3e8bfc39298ac71454a938a6
Avishkar <gandhiavishkar@gmail.com>
2024-02-14 13:33:20 +0530

    FinalCommit

---
+++ src/Details.js
@@ -0,0 +1,166 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/avi.png";
+import logo from "./assets/avi.png";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+import java from "./assets/techstack/java.png";
+import net from "./assets/techstack/net.jpeg"; // Corrected path
+import cpp from "./assets/techstack/cpp.png"; // Corrected path
+
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+// import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+// import projectImage4 from "./assets/projects/project4.jpg";
+// import projectImage5 from "./assets/projects/project5.jpg";
+// import projectImage6 from "./assets/projects/project6.jpg";
+import sortify from "./assets/projects/sortify.jpeg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Avishkar Gandhi",
+  tagline: "I build things for web",
+  img: profile,
+  about: `Hi there! I'm Avishkar, a passionate fullstack developer with experience in building responsive and interactive websites using MERN Stack`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/avishkar-gandhi",
+  github: "https://www.github.com/avishkar2001",
+  twitter: "https://twitter.com/gandhiavishkar",
+  instagram: "https://www.instagram.com/beawareofa5",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "SDE Intern",
+    Company: `Syngenta`,
+    Location: "Pune",
+    Type: "Internship",
+    Duration: "Aug 2022 - Feb 2023",
+  },
+  {
+    Position: "Research Intern",
+    Company: `Indian Army`,
+    Location: "Pune",
+    Type: "Internship",
+    Duration: "Sep 2021 - Feb 2022",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Bachelor in Electronics & Tele-Communication Engineering ",
+    Company: `Vishwakarma Institute of Information Technology`,
+    Location: "Pune",
+    Type: "Full Time",
+    Duration: "Aug 2019 - July 2023",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+  java: java,
+  net: net,
+  cpp: cpp,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Project title 1",
+    image: projectImage1,
+    description: `JigarWala Workout: Simplifying fitness routines with an easy-to-use MERN Stack application for streamlined and effective workouts.`,
+    techstack: "HTML/CSS, JavaScript, ReactJS, NodeJS, ExpressJS, MongoDB",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/avishkar2001/JigarWalaWorkOut",
+  },
+  {
+    title: "Sortify: Sorting made easy",
+    image: sortify,
+    description: `Sortify: Simplifying sorting algorithms with easy-to-understand visualizations.`,
+    techstack: "Java",
+    previewLink: "https://github.com/avishkar2001/Sortify",
+    githubLink: "https://github.com/avishkar2001/Sortify",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `ANPR: Achieve seamless Automatic Number Plate Detection with Python, Flask, and API integration for efficient license plate recognition.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://github.com/avishkar2001/ANPR",
+    githubLink: "https://github.com/avishkar2001/ANPR",
+  },
+  //   {
+  //     title: "Project title 4",
+  //     image: projectImage4,
+  //     description: `This is sample project description random things are here in description This is sample
+  // project lorem ipsum generator for dummy content`,
+  //     techstack: "HTML/CSS, JavaScript",
+  //     previewLink: "https://google.com",
+  //     githubLink: "https://github.com",
+  //   },
+  //   {
+  //     title: "Project title 5",
+  //     image: projectImage5,
+  //     description: `This is sample project description random things are here in description This is sample
+  // project lorem ipsum generator for dummy content`,
+  //     techstack: "HTML/CSS, JavaScript",
+  //     previewLink: "https://google.com",
+  //     githubLink: "https://github.com",
+  //   },
+  //   {
+  //     title: "Project title 6",
+  //     image: projectImage6,
+  //     description: `This is sample project description random things are here in description This is sample
+  // project lorem ipsum generator for dummy content`,
+  //     techstack: "HTML/CSS, JavaScript",
+  //     previewLink: "https://google.com",
+  //     githubLink: "https://github.com",
+  //   },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "avishkaroffice01@gmail.com",
+  phone: "+91 8080964160",
+};
